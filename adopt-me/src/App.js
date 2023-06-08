a033d04f1aabbf4abef7542076692d9213da64a6
leafoffaith <shaurya26@gmail.com>
2023-06-09 00:26:32 +0530

    Added ESLint config

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -2,22 +2,30 @@ const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
+        React.createElement("h2", {}, props.breed),
     ]);
-}
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-            React.createElement(Pet, { name: "Nico", animal: "Dog", breed: "Pekingese" }),
-            React.createElement(Pet, { name: "Roscoe", animal: "Dog", breed: "Retriever" }),
-        ]
-    );
-}
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+        }),
+        React.createElement(Pet, {
+            name: "Nico",
+            animal: "Dog",
+            breed: "Pekingese",
+        }),
+        React.createElement(Pet, {
+            name: "Roscoe",
+            animal: "Dog",
+            breed: "Retriever",
+        }),
+    ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
