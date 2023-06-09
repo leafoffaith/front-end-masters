f9b86955683f0a436cbbd05a12107d68e8f7cf31
leafoffaith <shaurya26@gmail.com>
2023-06-09 15:27:43 +0530

    Added SearchParams and Hooks

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
-import {createRoot} from 'react-dom';
-
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
+import {createRoot} from 'react-dom/client';
+import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "Havanese",
-        }),
-        React.createElement(Pet, {
-            name: "Nico",
-            animal: "Dog",
-            breed: "Pekingese",
-        }),
-        React.createElement(Pet, {
-            name: "Roscoe",
-            animal: "Dog",
-            breed: "Retriever",
-        }),
-    ]);
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+
+    );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
---
+++ adopt-me/src/SearchParams.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
