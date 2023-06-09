eca3d728c38d49ddcf39a65184f95a81183600e6
leafoffaith <shaurya26@gmail.com>
2023-06-09 21:45:31 +0530

    Pet stylized

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -1,21 +1,25 @@
-// import React from "react";
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed),
-//     ]);
-// };
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com.pets/none.jpg";
 
+  if (images.length) {
+    hero = images[0];
+  }
 
-const Pet = (props) => {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <h2>{props.animal}</h2>
-            <h2>{props.breed}</h2>
+  return (
+    <>
+      <a href={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
         </div>
-    );
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </a>
+    </>
+  );
 };
 
 //default is used to export a single value from a module
