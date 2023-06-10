b0f0f8901548abb71af2d57cf023607497f5aaf9
leafoffaith <shaurya26@gmail.com>
2023-06-10 14:25:29 +0530

    Details page added

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import Pet from './Pet';
-import SearchParams from './SearchParams';
+import React from "react";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Adopt Me!</h1>
-            <SearchParams />
-        </div>
-
-    );
+  return (
+    // Browser router makes context available
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <div>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
